b931ff2d88311870a6a76ed0a0468114412d6c7a
nwolfe <nikwolfe7@gmail.com>
2015-12-13 19:12:15 -0500

    asdfasdfasdfasdf

--- src/clustering/Cluster.java
+++ src/clustering/Cluster.java
@@ -8,167 +8,173 @@ import java.util.Map;
 
 public class Cluster {
 
-	private static int clusterIdGen = 1;
-	private List<DataInstance> clusterData;
-	private List<DataInstance> updateList;
-	private DistanceMetric distanceMetric;
-	private double[] centroid;
-	private int clusterId;
-	private double splitDeviation = 0.025;
+  private static int clusterIdGen = 1;
 
-	public Cluster(DistanceMetric distanceMetric) {
-		this.clusterId = clusterIdGen++; /*default*/
-		this.distanceMetric = distanceMetric;
-		this.clusterData = new LinkedList<>();
-		this.updateList = new LinkedList<>();
-		setCentroid(null);
-	}
+  private List<DataInstance> clusterData;
 
-	public Cluster(double[] centroid, DistanceMetric distanceMetric) {
-		this.clusterId = clusterIdGen++; /*default*/
-		this.distanceMetric = distanceMetric;
-		this.clusterData = new LinkedList<>();
-		this.updateList = new LinkedList<>();
-		setCentroid(centroid);
-	}
+  private List<DataInstance> updateList;
 
-	public int getClusterId() {
-		return clusterId;
-	}
-	
-	public double[] getCentroid() {
-		return centroid;
-	}
+  private DistanceMetric distanceMetric;
 
-	public DistanceMetric getDistanceMetric() {
-		return distanceMetric;
-	}
-	
-	public void setClusterId(int id) {
-		clusterId = id;
-	}
-	
-	public int pickClusterLabelFromData() {
-		Iterator<DataInstance> iter = getDataInstances();
-		Map<Double, Integer> counter = new HashMap<Double, Integer>();
-		double maxLabel = 0;
-		int maxCount = 0;
-		while (iter.hasNext()) {
-			DataInstance inst = iter.next();
-			double label = inst.getLabelValue()[0];
-			int count;
-			if (counter.containsKey(label)) {
-				count = counter.get(label) + 1;
-				counter.put(label, count);
-			} else {
-				count = 1;
-				counter.put(label, count);
-			}
-			if(count > maxCount) {
-				maxCount = count;
-				maxLabel = label;
-			}
-		}
-		return (int) maxLabel;
-	}
+  private double[] centroid;
 
-	private void setCentroid(double[] centroid) {
-		this.centroid = centroid;
-	}
+  private int clusterId;
 
-	public void initializeCentroid(DataInstance seed) {
-		setCentroid(seed.getDataVector());
-		addDataInstance(seed);
-	}
+  private double splitDeviation = 0.025;
 
-	public void addToClusterUpdate(DataInstance instance) {
-		updateList.add(instance);
-	}
+  public Cluster(DistanceMetric distanceMetric) {
+    this.clusterId = clusterIdGen++; /* default */
+    this.distanceMetric = distanceMetric;
+    this.clusterData = new LinkedList<>();
+    this.updateList = new LinkedList<>();
+    setCentroid(null);
+  }
 
-	public void update() {
-		Iterator<DataInstance> iter = updateList.iterator();
-		while (iter.hasNext()) {
-			addDataInstance(iter.next());
-			iter.remove();
-		}
-	}
+  public Cluster(double[] centroid, DistanceMetric distanceMetric) {
+    this.clusterId = clusterIdGen++; /* default */
+    this.distanceMetric = distanceMetric;
+    this.clusterData = new LinkedList<>();
+    this.updateList = new LinkedList<>();
+    setCentroid(centroid);
+  }
 
-	public void addDataInstance(DataInstance instance) {
-		clusterData.add(instance);
-	}
+  public int getClusterId() {
+    return clusterId;
+  }
 
-	public boolean removeDataInstance(DataInstance instance) {
-		boolean returned = clusterData.remove(instance); 
-	  return returned;
-	}
+  public double[] getCentroid() {
+    return centroid;
+  }
 
-	public Iterator<DataInstance> getDataInstances() {
-		return clusterData.iterator();
-	}
+  public DistanceMetric getDistanceMetric() {
+    return distanceMetric;
+  }
 
-	public int getClusterSize() {
-		return clusterData.size();
-	}
+  public void setClusterId(int id) {
+    clusterId = id;
+  }
 
-	public void drainCluster() {
-		clusterData.clear();
-		calculateCentroid();
-	}
+  public int pickClusterLabelFromData() {
+    Iterator<DataInstance> iter = getDataInstances();
+    Map<Double, Integer> counter = new HashMap<Double, Integer>();
+    double maxLabel = 0;
+    int maxCount = 0;
+    while (iter.hasNext()) {
+      DataInstance inst = iter.next();
+      double label = inst.getLabelValue()[0];
+      int count;
+      if (counter.containsKey(label)) {
+        count = counter.get(label) + 1;
+        counter.put(label, count);
+      } else {
+        count = 1;
+        counter.put(label, count);
+      }
+      if (count > maxCount) {
+        maxCount = count;
+        maxLabel = label;
+      }
+    }
+    return (int) maxLabel;
+  }
 
-	public void combineCluster(Cluster cluster) {
-		/* choose the lower valued id */
-		int id1, id2;
-		id1 = getClusterId();
-		id2 = cluster.getClusterId();
-		clusterId = id1 < id2 ? id1 : id2;
-		Iterator<DataInstance> iter = cluster.getDataInstances();
-		while (iter.hasNext()) {
-			DataInstance instance = iter.next();
-			addDataInstance(instance);
-			iter.remove();
-		}
-		/* destroy other cluster */
-		cluster = null;
-		calculateCentroid();
-	}
+  private void setCentroid(double[] centroid) {
+    this.centroid = centroid;
+  }
 
-	public Cluster splitCluster() {
-		double[] cent1, cent2, curr;
-		curr = getCentroid();
-		cent1 = new double[curr.length];
-		cent2 = new double[curr.length];
-		/* perturb centroid by 5% */
-		for (int i = 0; i < curr.length; i++) {
-			cent1[i] = curr[i] + (curr[i] * splitDeviation);
-			cent2[i] = curr[i] - (curr[i] * splitDeviation);
-		}
-		setCentroid(cent1);
-		return new Cluster(cent2, getDistanceMetric());
-	}
+  public void initializeCentroid(DataInstance seed) {
+    setCentroid(seed.getDataVector());
+    addDataInstance(seed);
+  }
 
-	/* returns the amount the centroid changed */
-	public double calculateCentroid() {
-		update();
-		double[] newCentroid = new double[getCentroid().length];
-		Iterator<DataInstance> iter = getDataInstances();
-		while (iter.hasNext()) {
-			DataInstance instance = iter.next();
-			for (int i = 0; i < instance.getDataDimension(); i++) {
-				newCentroid[i] += instance.getDataVector()[i];
-			}
-		}
-		int size = getClusterSize();
-		double divisor = (size > 0) ? 1.0 / size : size;
-		for (int i = 0; i < newCentroid.length; i++) {
-			newCentroid[i] *= divisor;
-		}
-		double diff = distanceMetric.getDistance(getCentroid(), newCentroid);
-		setCentroid(newCentroid);
-		return diff;
-	}
+  public void addToClusterUpdate(DataInstance instance) {
+    updateList.add(instance);
+  }
 
-	public double getCentroidDistance(DataInstance instance) {
-		return distanceMetric.getDistance(getCentroid(), instance.getDataVector());
-	}
+  public void update() {
+    Iterator<DataInstance> iter = updateList.iterator();
+    while (iter.hasNext()) {
+      addDataInstance(iter.next());
+      iter.remove();
+    }
+  }
+
+  public void addDataInstance(DataInstance instance) {
+    clusterData.add(instance);
+  }
+
+  public boolean removeDataInstance(DataInstance instance) {
+    boolean returned = clusterData.remove(instance);
+    return returned;
+  }
+
+  public Iterator<DataInstance> getDataInstances() {
+    return clusterData.iterator();
+  }
+
+  public int getClusterSize() {
+    return clusterData.size();
+  }
+
+  public void drainCluster() {
+    clusterData.clear();
+    calculateCentroid();
+  }
+
+  public void combineCluster(Cluster cluster) {
+    /* choose the lower valued id */
+    int id1, id2;
+    id1 = getClusterId();
+    id2 = cluster.getClusterId();
+    clusterId = id1 < id2 ? id1 : id2;
+    Iterator<DataInstance> iter = cluster.getDataInstances();
+    while (iter.hasNext()) {
+      DataInstance instance = iter.next();
+      addDataInstance(instance);
+      iter.remove();
+    }
+    /* destroy other cluster */
+    cluster = null;
+    calculateCentroid();
+  }
+
+  public Cluster splitCluster() {
+    double[] cent1, cent2, curr;
+    curr = getCentroid();
+    cent1 = new double[curr.length];
+    cent2 = new double[curr.length];
+    /* perturb centroid by 5% */
+    for (int i = 0; i < curr.length; i++) {
+      cent1[i] = curr[i] + (curr[i] * splitDeviation);
+      cent2[i] = curr[i] - (curr[i] * splitDeviation);
+    }
+    setCentroid(cent1);
+    return new Cluster(cent2, getDistanceMetric());
+  }
+
+  /* returns the amount the centroid changed */
+  public double calculateCentroid() {
+    update();
+    double[] newCentroid = new double[getCentroid().length];
+    Iterator<DataInstance> iter = getDataInstances();
+    while (iter.hasNext()) {
+      DataInstance instance = iter.next();
+      for (int i = 0; i < instance.getDataDimension(); i++) {
+        newCentroid[i] += instance.getDataVector()[i];
+      }
+    }
+    int size = getClusterSize();
+    double divisor = (size > 0) ? 1.0 / size : size;
+    for (int i = 0; i < newCentroid.length; i++) {
+      newCentroid[i] *= divisor;
+    }
+    double diff = distanceMetric.getDistance(getCentroid(), newCentroid);
+    setCentroid(newCentroid);
+    return diff;
+  }
+
+  public double getCentroidDistance(DataInstance instance) {
+    return distanceMetric.getDistance(getCentroid(), instance.getDataVector());
+  }
 
 }
--- src/clustering/KMeansClustering.java
+++ src/clustering/KMeansClustering.java
@@ -1,184 +1,199 @@
 package clustering;
 
-import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
 public class KMeansClustering {
 
-	private List<Cluster> clusters;
-	private DistanceMetric distMetric;
-	private double targetCentroidDifference = 1.0e-10;
-	private int maxIterations = 100000;
-	private int K;
+  private List<Cluster> clusters;
 
-	public KMeansClustering(int K, DistanceMetric metric) {
-		this.K = K;
-		this.distMetric = metric;
-		this.clusters = new LinkedList<>();
-		for (int i = 0; i < K; i++) {
-			clusters.add(new Cluster(metric));
-		}
-	}
+  private DistanceMetric distMetric;
 
-	public DistanceMetric getDistanceMetric() {
-		return distMetric;
-	}
+  private boolean outputOn;
 
-	public void setConvergenceCriteria(double minDiff, int maxIterations) {
-		this.targetCentroidDifference = minDiff;
-		this.maxIterations = maxIterations;
-	}
+  private double targetCentroidDifference = 1.0e-10;
 
-	public void doKMeansClustering(ClusterDataSet dataset) {
-		System.out.println("Randomly initializing K-means with " + K + " clusters... ");
-		List<DataInstance> data = dataset.getClusteringData();
-		/* Initialize clusters */
-		spreadData(data);
-		/* Calculate initial centroids... */
-		calcCentroids();
-		/* Begin iterating */
-		int iteration = 1;
-		double prevDiff = Double.POSITIVE_INFINITY;
-		/* START */
-		while (true) {
-			/* Reassign Clusters */
-			for (int i = 0; i < getKClusters().size(); i++) {
-				/* For each cluster... */
-				Cluster cluster = getKClusters().get(i);
-				/* Best cluster assignment pointer */
-				Cluster bestCluster = cluster;
-				Iterator<DataInstance> iter = cluster.getDataInstances();
-				/* For each data point in the cluster... */
-				while (iter.hasNext()) {
-					DataInstance instance = iter.next();
-					/* Calculate the distance to the current cluster centroid... */
-					double minDistance = cluster.getCentroidDistance(instance);
-					for (int j = 0; j < getKClusters().size(); j++) {
-						/* Now check the other centroids */
-						if (i != j) {
-							Cluster kCluster = getKClusters().get(j);
-							/* Check distance to other centroid... */
-							double dist = kCluster.getCentroidDistance(instance);
-							/* Check if better... */
-							if (dist < minDistance) {
-								minDistance = dist;
-								bestCluster = kCluster;
-							}
-						}
-					}
-					/* Reassign cluster if necessary */
-					if (bestCluster != cluster) {
-						iter.remove();
-						bestCluster.addToClusterUpdate(instance);
-					}
-				}
-			}
-			/* Recalculate Centroids */
-			double centroidChange = calcCentroids();
-			/* Fix dead clusters... */
-			for(int i = 0; i < getKClusters().size(); i++) {
-				Cluster cluster = getKClusters().get(i);
-				if(cluster.getClusterSize() == 0) {
-					DataInstance newCentroid;
-					newCentroid = getFarthestDataPoint();
-					cluster.initializeCentroid(newCentroid);
-				}
-			}
-			/* Update the world... */
-			double diff = prevDiff - centroidChange;
-			System.out.println("Iteration " + iteration + " | Total Centroid Change: " + centroidChange + " Diff: " + diff);
-			/* Update Diff */
-			prevDiff = centroidChange;
-			/* Check stopping criteria... */
-			if (maxIterations > 0 && iteration++ >= maxIterations)
-				break;
-			if (targetCentroidDifference > 0 && Math.abs(diff) <= targetCentroidDifference) 
-				break;
-		}
-		System.out.println("K-Means Clustering Complete! ");
-		calculateAccuracy();
-	}
-	
-	/*
-	 * Returns the data point with the largest
-	 * minimum distance from any centroid, i.e.
-	 * the most "distant" datapoint
-	 * 
-	 * */
-	private DataInstance getFarthestDataPoint() {
-		Cluster farCluster = null;
-		DataInstance farInstance = null;
-		double farDist = 0;
-		for(Cluster cluster : getKClusters()) {
-		  Iterator<DataInstance> iter = cluster.getDataInstances();
-		  while(iter.hasNext()) {
-		    DataInstance inst = iter.next();
-		    Cluster minDistCluster = cluster;
-		    double minDist = Double.POSITIVE_INFINITY;
-		    for(Cluster kCluster : getKClusters()) {
-		      double dist = kCluster.getCentroidDistance(inst);
-		      if(dist < minDist) {
-		        minDist = dist;
-		        minDistCluster = cluster;
-		      }
-		    }
-		    if(minDist > farDist) {
-		      farDist = minDist;
-		      farInstance = inst;
-		      farCluster = minDistCluster;
-		    }
-		  }
-		}
-		farCluster.removeDataInstance(farInstance);
-		return farInstance;
-	}
+  private int maxIterations = 100000;
 
-	private void calculateAccuracy() {
-		int[][] cMat = new int[K][K];
-		for(Cluster cluster : getKClusters()) {
-			int clusterLabel = cluster.pickClusterLabelFromData();
-			System.out.println("Most common label: " + clusterLabel);
-			Iterator<DataInstance> iter = cluster.getDataInstances();
-			while(iter.hasNext()) {
-				DataInstance inst = iter.next();
-				int instLabel = (int) inst.getLabelValue()[0];
-				cMat[clusterLabel-1][instLabel-1] += 1;
-			}
-		}
-		StringBuilder sb = new StringBuilder("\n");
-		for(int i = 0; i < cMat.length; i++) {
-			for(int j = 0; j < cMat[0].length; j++) {
-				sb.append(cMat[i][j] + "\t");
-			}
-			sb.append("\n");
-		}
-		System.out.println(sb.toString());
-	}
-	
-	private void spreadData(List<DataInstance> data) {
-		//Collections.shuffle(data);
-		int i = 0;
-		for (DataInstance instance : data) {
-			Cluster cluster = getKClusters().get(i);
-			cluster.addDataInstance(instance);
-			if (cluster.getCentroid() == null)
-				cluster.initializeCentroid(instance);
-			i = (i + 1) % K;
-		}
-	}
+  private int K;
 
-	private double calcCentroids() {
-		double centroidChange = 0;
-		for (Cluster cluster : getKClusters()) {
-			centroidChange += cluster.calculateCentroid();
-		}
-		return centroidChange;
-	}
+  public KMeansClustering(int K, DistanceMetric metric) {
+    this.K = K;
+    this.distMetric = metric;
+    this.clusters = new LinkedList<>();
+    for (int i = 0; i < K; i++) {
+      clusters.add(new Cluster(metric));
+    }
+    this.outputOn = false;
+  }
 
-	public List<Cluster> getKClusters() {
-		return clusters;
-	}
+  public DistanceMetric getDistanceMetric() {
+    return distMetric;
+  }
+
+  public void setConvergenceCriteria(double minDiff, int maxIterations) {
+    this.targetCentroidDifference = minDiff;
+    this.maxIterations = maxIterations;
+  }
+
+  public void doKMeansClustering(ClusterDataSet dataset) {
+    System.out.println("Randomly initializing K-means with " + K + " clusters... ");
+    List<DataInstance> data = dataset.getClusteringData();
+    /* Initialize clusters */
+    spreadData(data);
+    /* Calculate initial centroids... */
+    calcCentroids();
+    /* Begin iterating */
+    int iteration = 1;
+    double prevDiff = Double.POSITIVE_INFINITY;
+    /* START */
+    while (true) {
+      /* Reassign Clusters */
+      for (int i = 0; i < getKClusters().size(); i++) {
+        /* For each cluster... */
+        Cluster cluster = getKClusters().get(i);
+        /* Best cluster assignment pointer */
+        Cluster bestCluster = cluster;
+        Iterator<DataInstance> iter = cluster.getDataInstances();
+        /* For each data point in the cluster... */
+        while (iter.hasNext()) {
+          DataInstance instance = iter.next();
+          /* Calculate the distance to the current cluster centroid... */
+          double minDistance = cluster.getCentroidDistance(instance);
+          for (int j = 0; j < getKClusters().size(); j++) {
+            /* Now check the other centroids */
+            if (i != j) {
+              Cluster kCluster = getKClusters().get(j);
+              /* Check distance to other centroid... */
+              double dist = kCluster.getCentroidDistance(instance);
+              /* Check if better... */
+              if (dist < minDistance) {
+                minDistance = dist;
+                bestCluster = kCluster;
+              }
+            }
+          }
+          /* Reassign cluster if necessary */
+          if (bestCluster != cluster) {
+            iter.remove();
+            bestCluster.addToClusterUpdate(instance);
+          }
+        }
+      }
+      /* Recalculate Centroids */
+      double centroidChange = calcCentroids();
+      /* Fix dead clusters... */
+      for (int i = 0; i < getKClusters().size(); i++) {
+        Cluster cluster = getKClusters().get(i);
+        if (cluster.getClusterSize() == 0) {
+          DataInstance newCentroid;
+          newCentroid = getFarthestDataPoint();
+          cluster.initializeCentroid(newCentroid);
+        }
+      }
+      /* Update the world... */
+      double diff = prevDiff - centroidChange;
+      if (outputOn)
+        System.out.println("Iteration " + iteration + " | Total Centroid Change: " + centroidChange + " Diff: " + diff);
+      /* Update Diff */
+      prevDiff = centroidChange;
+      /* Check stopping criteria... */
+      if (maxIterations > 0 && iteration++ >= maxIterations)
+        break;
+      if (targetCentroidDifference > 0 && Math.abs(diff) <= targetCentroidDifference)
+        break;
+    }
+    System.out.println("K-Means Clustering Complete! ");
+    calculateAccuracy();
+  }
+
+  /*
+   * Returns the data point with the largest minimum distance from any centroid, i.e. the most
+   * "distant" datapoint
+   */
+  private DataInstance getFarthestDataPoint() {
+    Cluster farCluster = null;
+    DataInstance farInstance = null;
+    double farDist = 0;
+    for (Cluster cluster : getKClusters()) {
+      Iterator<DataInstance> iter = cluster.getDataInstances();
+      while (iter.hasNext()) {
+        DataInstance inst = iter.next();
+        Cluster minDistCluster = cluster;
+        double minDist = Double.POSITIVE_INFINITY;
+        for (Cluster kCluster : getKClusters()) {
+          double dist = kCluster.getCentroidDistance(inst);
+          if (dist < minDist) {
+            minDist = dist;
+            minDistCluster = cluster;
+          }
+        }
+        if (minDist > farDist) {
+          farDist = minDist;
+          farInstance = inst;
+          farCluster = minDistCluster;
+        }
+      }
+    }
+    farCluster.removeDataInstance(farInstance);
+    return farInstance;
+  }
+
+  private void calculateAccuracy() {
+    int[][] cMat = new int[K][K];
+    for (Cluster cluster : getKClusters()) {
+      int clusterLabel = cluster.pickClusterLabelFromData();
+      if(outputOn)
+        System.out.println("Most common label: " + clusterLabel);
+      Iterator<DataInstance> iter = cluster.getDataInstances();
+      while (iter.hasNext()) {
+        DataInstance inst = iter.next();
+        int instLabel = (int) inst.getLabelValue()[0];
+        cMat[clusterLabel - 1][instLabel - 1] += 1;
+      }
+    }
+    StringBuilder sb = new StringBuilder("\nlabel\t");
+    String s1, s2;
+    s1 = "";
+    s2 = "";
+    for(int k = 0; k < K; k++) {
+      s1 += "" + (k+1) + "\t";
+      s2 += "________";
+    }
+    sb.append(s1 + "\n" + s2 + "___\n");
+    for (int i = 0; i < cMat.length; i++) {
+      sb.append((i+1) + " | \t");
+      for (int j = 0; j < cMat[0].length; j++) {
+        sb.append(cMat[i][j] + "\t");
+      }
+      sb.append("\n");
+    }
+    System.out.println(sb.toString());
+  }
+
+  private void spreadData(List<DataInstance> data) {
+    // Collections.shuffle(data);
+    int i = 0;
+    for (DataInstance instance : data) {
+      Cluster cluster = getKClusters().get(i);
+      cluster.addDataInstance(instance);
+      if (cluster.getCentroid() == null)
+        cluster.initializeCentroid(instance);
+      i = (i + 1) % K;
+    }
+  }
+
+  private double calcCentroids() {
+    double centroidChange = 0;
+    for (Cluster cluster : getKClusters()) {
+      centroidChange += cluster.calculateCentroid();
+    }
+    return centroidChange;
+  }
+
+  public List<Cluster> getKClusters() {
+    return clusters;
+  }
 
 }
